7cbadbd6d5fb082d1e43af22787ba3359cfea474
Sin.k <miraelsink@gmail.com>
2024-12-12 16:25:58 +0300

    fix data race

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -19,16 +19,16 @@ void eats(t_philo *philo)
     table = philo->table;
     if (philo->philo_id % 2 == 0)
     {
-        pthread_mutex_lock(&(table->lock_fork[philo->left_fork_id]));
+        pthread_mutex_lock(&(table->lock_fork[philo->left_fork_id - 1]));
         print_task(table, philo->philo_id, "has taken a fork");
-        pthread_mutex_lock(&(table->lock_fork[philo->right_fork_id]));
+        pthread_mutex_lock(&(table->lock_fork[philo->right_fork_id - 1]));
         print_task(table, philo->philo_id, "has taken a fork");
     }
     else
     {
-        pthread_mutex_lock(&(table->lock_fork[philo->right_fork_id]));
+        pthread_mutex_lock(&(table->lock_fork[philo->right_fork_id - 1]));
         print_task(table, philo->philo_id, "has taken a fork");
-        pthread_mutex_lock(&(table->lock_fork[philo->left_fork_id]));
+        pthread_mutex_lock(&(table->lock_fork[philo->left_fork_id - 1]));
         print_task(table, philo->philo_id, "has taken a fork");
     }
     pthread_mutex_lock(&(table->check_meal));
@@ -37,8 +37,8 @@ void eats(t_philo *philo)
     pthread_mutex_unlock(&(table->check_meal));
     smart_sleep(table->time_eat, table);
     (philo->x_ate)++;
-    pthread_mutex_unlock(&(table->lock_fork[philo->left_fork_id]));
-    pthread_mutex_unlock(&(table->lock_fork[philo->right_fork_id]));
+    pthread_mutex_unlock(&(table->lock_fork[philo->left_fork_id - 1]));
+    pthread_mutex_unlock(&(table->lock_fork[philo->right_fork_id - 1]));
 }
 
 void    sleeping(unsigned int id, t_table *table)
@@ -56,6 +56,8 @@ void    *routine(void *void_routine)
     i = 0;
     philo = (t_philo *)void_routine;
     table = philo->table;
+    if (philo->philo_id % 2 == 0)
+        usleep(500);
     while(!(table->died))
     {
         eats(philo);
@@ -63,6 +65,8 @@ void    *routine(void *void_routine)
             break ;
         sleeping(philo->philo_id, table);
         print_task(table, philo->philo_id, "is thinking");
+        // usleep(500);
+        smart_sleep(((table->time_eat) - (table->time_sleep)) / 2, table);
         i++;
     }
     return (NULL);
@@ -78,10 +82,11 @@ int launch(t_table *table)
     table->first_timestamp = get_time();
     while (i < table->nb_philo)
     {
-        printf("%d\n", i);
         if (pthread_create(&(philo[i].thread_id), NULL, routine, &(philo[i])))
             return (1);
+        pthread_mutex_lock(&(table->check_meal));
         philo->last_meal = get_time();
+        pthread_mutex_unlock(&(table->check_meal));
         i++;
     }
     i = -1;
